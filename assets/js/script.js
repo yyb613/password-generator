b99bc930d643c101ac8998192dd0f77ee58b0ec0
Yaakov Yisrael Benjamin <yyisrael613@gmail.com>
2022-01-20 23:23:24 -0500

    created password generator function

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,31 +5,74 @@ function numberOfChars() {
   while (numChar < 8 || numChar > 128) {
     numChar = prompt("Choose number of characters between 8-128"); // User chooses # of characters
   }
+
+  return numChar;
 }
 
-// Character type declarations
-var lower;
-var upper;
-var numbers;
-var special;
+// Character type object declaration
+var charTypeObj = {};
 
 // Character type helper function
 function charTypeSelect() {
-  lower = confirm("Would you like lowercase letters in your password?"); // User decides lowercase letters
-  upper = confirm("How about uppercase letters?");                       // User decides uppercase letters
-  numbers = confirm("What about numbers?");                              // User decides numbers
-  special = confirm("Any special characters?");                          // User decides special characters
+  charTypeObj = {
+    lower : confirm("Would you like lowercase letters in your password?"), // User decides lowercase letters
+    upper : confirm("How about uppercase letters?"),                       // User decides uppercase letters
+    numbers : confirm("What about numbers?"),                              // User decides numbers
+    special : confirm("Any special characters?"),                          // User decides special characters 
+  };
 
-  while (lower === false && upper === false && numbers === false && special === false) { // If no character type is selected
-    alert("You must select at least one character type! \nPlease try again.");
-    charTypeSelect();
+  while ( // If no character type is selected
+    charTypeObj["lower"] === false && 
+    charTypeObj["upper"] === false && 
+    charTypeObj["numbers"] === false && 
+    charTypeObj["special"] === false
+    ) { 
+    alert("You must select at least one character type! \nPlease try again."); // Notify user
+    charTypeSelect(); // Rerun function
   }
+
+  return charTypeObj;
 }
+
+// Initialize password characters variables
+var lowerChars = "abcdefghijklmnopqrstuvwxyz";
+var upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXWZ";
+var nums = "123456789";
+var specials = " !\"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~";
+var selectedTypes = '';
 
 // Password Generator function
 function generatePassword() {
-  numberOfChars(); // User chooses # of chars
-  charTypeSelect(); // User chooses character types
+  var numChar = numberOfChars(); // User chooses # of chars
+  var charTypeObj = charTypeSelect(); // User chooses character types
+  
+  // Reset selectedTypes
+  selectedTypes = '';
+
+  // Append character types
+  if (charTypeObj["lower"]) {
+    selectedTypes += lowerChars; // appends lowercase letters
+  }
+
+  if (charTypeObj["upper"]) {
+    selectedTypes += upperChars; // appends uppercase letters
+  }
+
+  if (charTypeObj["numbers"]) {
+    selectedTypes += nums; // appends numbers
+  }
+
+  if (charTypeObj["special"]) {
+    selectedTypes += specials; // appends special characters
+  }
+
+  var password = '';
+
+  for (var i = 0; i < numChar; i++) { // iterates "numChar" times
+    password += selectedTypes[Math.floor(Math.random() * selectedTypes.length)]; // appends random char to new password
+  }
+
+  return password; // returns generated password
 }
 
 // Write password to the #password input
@@ -46,15 +89,6 @@ var generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", writePassword);
 
 /*
-WHEN all prompts are answered
-THEN a password is generated that matches the selected criteria
-
-WHEN the password is generated
-THEN the password is either displayed in an alert or written to the page
-
 No errors in console
-
-comments
-
 README w/description, screenshot, and link to deployed application
 */
